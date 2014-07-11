f94ba9e9a7e2e40516fc7c7256896641595f800f
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-07-10 21:52:38 -0500

    adding comments

--- pandoc_filter.py
+++ pandoc_filter.py
@@ -4,9 +4,13 @@
 from pandocfilters import toJSONFilter
 
 def process(key, value, format, meta):
+
+	# find images and change their src paths to relative ones
 	if key == 'Image':
 		src = value[1][0]
 		value[1][0] = '../images/' + src.split('/')[-1]
+
+	# find links to old uploads and change their href paths to relative ones
 	if key == 'Link':
 		href = value[1][0]
 		if 'programminghistorian.org/wp-content/uploads/' in href:
