ea91c8c4d06273aaccff70102f0b013425a3fb07
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-04-13 20:23:10 -0500

    Fix style declaration on fa-link

--- js/header_links.js
+++ js/header_links.js
@@ -5,7 +5,7 @@ $(function() {
     var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
-    icon = '<i class="fa fa-link" style="fontsize: 0.8em"></i>';
+    icon = '<i class="fa fa-link" style="font-size: 0.8em"></i>';
     if (id) {
       return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
