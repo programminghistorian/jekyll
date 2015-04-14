7d3a6374b6010e6dc8c3fddd9eb071d88955335c
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-04-13 19:59:56 -0500

    Add hover link icons to headers; closes #79

---
+++ js/header_links.js
@@ -0,0 +1,15 @@
+// http://ben.balter.com/2014/03/13/pages-anchor-links/
+
+<script>
+$(function() {
+  return $("h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="fa fa-link"></i>';
+    if (id) {
+      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
+</script>
