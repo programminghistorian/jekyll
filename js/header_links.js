1944bf2b31f9fea22807bffd8e507bb0a0509bd9
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-04-13 20:14:42 -0500

    Adjust rotate and size of anchor header link

--- js/header_links.js
+++ js/header_links.js
@@ -5,7 +5,7 @@ $(function() {
     var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
-    icon = '<i class="fa fa-link fa-rotate-135"></i>';
+    icon = '<i class="fa fa-link fa-lg fa-rotate-45"></i>';
     if (id) {
       return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
