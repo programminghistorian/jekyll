2fa4c3985d65df7e41f282a68e7c6a84e6761d05
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-04-13 20:11:35 -0500

    Rotate header-link anchor icon

--- js/header_links.js
+++ js/header_links.js
@@ -5,7 +5,7 @@ $(function() {
     var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
-    icon = '<i class="fa fa-link"></i>';
+    icon = '<i class="fa fa-link fa-rotate-135"></i>';
     if (id) {
       return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
