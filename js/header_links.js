29598403544b0fbb6a55d943c4d849a46df54588
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2015-04-29 16:25:07 -0500

    Revert to original placing of header link icon

--- js/header_links.js
+++ js/header_links.js
@@ -1,15 +1,13 @@
 // http://ben.balter.com/2014/03/13/pages-anchor-links/
 
 $(function() {
-  return $("h2, h3, h4, h5, h6").each(function() {
-    var icon, id;
-    id = this.attr('id');
+  return $("h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
     icon = '<i class="fa fa-link" style="font-size: 0.8em"></i>';
-    var div=$(document.createElement('div'));
-        div.setAttribute('style', 'float: right; font-size: 0.8em;')
-        if (id) {
-           return div.innerHTML($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
-        }
-    this.insertBefore(div, this.firstChild);
+    if (id) {
+      return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
   });
 });
