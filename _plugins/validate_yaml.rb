d93582dc11a7e95e226e63c9492391b933c18923
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-05-11 10:43:44 -0700

    Collect errors per page
    This tidies up the display so each page has its own error report

--- _plugins/validate_yaml.rb
+++ _plugins/validate_yaml.rb
@@ -3,20 +3,35 @@ module MyModule
   class WarningGenerator < Jekyll::Generator
     def generate(site)
 
-      errors = Array.new
+      total_errors = Array.new
 
-      site.pages.each do |p|
+      red = "\e[31m"
+      clear = "\e[0m"
+
+      lessons = site.pages.keep_if{|i| i.data["layout"] == "default"}
+
+      lessons.each do |p|
+
+        page_errors = Array.new
+
         if p.data['editors'].nil?
-           errors.push("On #{p.name}: editors is missing")
+          page_errors.push("'editors' is missing.")
+        end
+
+        if p.data["reviewers"].nil?
+          page_errors.push("'reviewers' is missing.")
+        end
+
+        unit_errors = page_errors.map{|e| "\t - #{e}"}.join("\n")
+
+        unless page_errors.empty?
+          warn "#{red}In #{p.dir}#{p.name}:\n#{unit_errors}#{clear}"
+          total_errors.concat(page_errors)
         end
       end
 
-      errors.each do |error|
-        warn error
-      end
-
-      unless errors.empty?
-        raise "There were YAML errors."
+      unless total_errors.empty?
+        raise "#{red}There were YAML errors.#{clear}"
       end
     end
   end
