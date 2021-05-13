60a10b85426697cb1f02283705ee61dae8d3dfc2
walshbr <bmw9t@virginia.edu>
2021-05-13 15:20:21 -0400

    Other contrast fixes and icons missing names

--- js/header_links.js
+++ js/header_links.js
@@ -5,9 +5,9 @@ $(function() {
     var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
-    icon = '<i class="fa fa-link" style="font-size: 0.8em"><span class="visually-hidden">link to this section</span></i>';
+    icon = '<i class="fa fa-link" style="font-size: 0.8em"></i>';
     if (id) {
-      return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+      return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon + '<span class="visually-hidden">' + id + '</span>'));
     }
   });
 });
